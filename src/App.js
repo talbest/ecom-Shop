26d823f2e91fb51fcdb77fccb8243c5ca2db59d3
TalBest <talbest94@gmail.com>
2022-01-14 23:02:11 +0200

    added collection and collection item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-
+import ShopPage from './component/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/component/manu-item/menu-item.component.jsx
+++ src/component/manu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div  className={`${size} menu-item`}>
-            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}/>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
@@ -12,4 +13,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
