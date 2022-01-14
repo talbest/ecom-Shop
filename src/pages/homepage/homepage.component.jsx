34d2baaad37d344352cce66d6369fbcb40ae2f5a
TalBest <talbest94@gmail.com>
2022-01-14 19:20:48 +0200

    added the directory compnent and the manu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../../App";
+import './homepage.styles.scss';
+import Directory from "../../component/directory/directory.component.jsx";
+const HomePage = () => (
+    <div className="homepage">
+      <Directory />
+    
+    </div>
+);
+
+export default HomePage;
