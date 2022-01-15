3f409c1ad69c52e7251cece33291bcbb8db9b936
TalBest <talbest94@gmail.com>
2022-01-15 22:22:58 +0200

    finished sign in

--- src/component/sign-in/sign-in.component.jsx
+++ src/component/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormInput from "../form-Input/form-input.component";
 import CostumeButton from "../costume-button/costume-button.component";
-import { signInWithGoogle } from '../../firebase/firebase.util'
+import { auth, signInWithGoogle } from '../../firebase/firebase.util'
 import './sign-in.styles.scss'
 
 class SignIn extends React.Component {
@@ -15,9 +15,15 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '', })
+        const { email, password } = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {
@@ -35,8 +41,8 @@ class SignIn extends React.Component {
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} required label="email" />
                     <FormInput name="Password" type="password" value={this.state.password} handleChange={this.handleChange} required label="password" />
                     <div className="buttons">
-                    <CostumeButton type='submit' value="submit form"> sign in</CostumeButton>
-                    <CostumeButton onClick={signInWithGoogle} value="submit form" isGooglesignIn> sign in with google </CostumeButton>
+                        <CostumeButton type='submit' value="submit form"> sign in</CostumeButton>
+                        <CostumeButton onClick={signInWithGoogle} value="submit form" isGooglesignIn> sign in with google </CostumeButton>
 
                     </div>
 
